179294401067bcdaaf83bd455aa31740860de84b
martinabt-dev <martin.abt.hst@gmail.com>
2026-04-23 18:56:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import streamlit as st
 
-
-retailers_df = pd.read_csv('C:/Users/User/Desktop/WBS/project_3/raw/retailers.csv', decimal=',')
+# import csv files as df
+retailers_df = pd.read_csv('raw/retailers.csv', decimal=',')
 methods_df = pd.read_csv('raw/methods.csv', decimal=',')
 daily_sales_df = pd.read_csv('raw/daily_sales.csv', decimal=',')
 products_df = pd.read_csv('raw/products.csv', decimal=',')
@@ -10,31 +10,48 @@ products_df = pd.read_csv('raw/products.csv', decimal=',')
 goexplore_df = daily_sales_df.merge(retailers_df, on='Retailer code', how='left')
 goexplore_df = goexplore_df.merge(methods_df, on='Order method code', how='left')
 goexplore_df = goexplore_df.merge(products_df, on='Product number', how='left')
-st.write(goexplore_df)
-
-# Revenue per Month
 goexplore_df['Date'] = pd.to_datetime(goexplore_df['Date'], dayfirst=True)
-result = goexplore_df.groupby(goexplore_df['Date'].dt.to_period('M'))['Unit price_x'].sum()
-result.index = result.index.to_timestamp()
-st.write("Revenue / month -> Unit price_x")
-st.line_chart(result)
+# st.write(goexplore_df)
 
-result = goexplore_df.groupby(goexplore_df['Date'].dt.to_period('M'))['Unit sale price'].sum()
-result.index = result.index.to_timestamp()
-st.write("Revenue / month -> Unit sale price")
-st.line_chart(result)
+st.set_page_config(layout="wide")
+col1, col2, col3 = st.columns(3)
+group = {
+    "Month" : goexplore_df['Date'].dt.to_period('M'),
+    "Quarter" : goexplore_df['Date'].dt.to_period('Q'),
+    "Year" : goexplore_df['Date'].dt.to_period('Y'),
+    "Country" : "Country",
+    "Retailer" : "Retailer name",
+    "Retailer Type" : "Type",
+    "Product" : "Product",
+    "Product type" : "Product type"
+}
 
-# Most popular product categories
-st.write("Most popular product categories")
-pop_product_cat = goexplore_df.groupby(goexplore_df['Product type'])["Quantity"].sum()
-st.write(pop_product_cat.sort_values(ascending=False))
+agg = {
+    "Min" : "min",
+    "Max" : "max",
+    "Count" : "count",
+    "Sum" : "sum",
+    "Median" : "median",
+    "Mean" : "mean"
+}
 
-# Countries with the highest return rates
-st.write("Countries with the highest return rates")
+columns = {
+    "Quantity" : "Quantity",
+    "Unit price" : "Unit price_x",
+    "Unit sale price" : "Unit sale price"
+}
 
-# Countries with most orders
-st.write("Countries with most orders")
-pop_product_cat = goexplore_df.groupby(goexplore_df['Country'])["Product number"].count()
-st.write(pop_product_cat.sort_values(ascending=False))
+with col1:
+    group_choice = st.selectbox("Group", group)
 
-st.write(f"Number of Retailer: {goexplore_df['Retailer code'].nunique()}")
+with col2:
+    agg_choice = st.selectbox("Aggregation", agg)
+
+with col3:
+    column_choice = st.selectbox("Column", columns)
+
+result = goexplore_df.groupby(group[group_choice]).agg(choice = (columns[column_choice], agg[agg_choice]))
+st.write(result)
+
+if group_choice in ["Month", "Year", "Quarter"]:
+    st.line_chart(result)
