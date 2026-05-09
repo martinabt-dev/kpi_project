80e01b3a47fbfb2be7c67b1cda227d9ca4240994
martinabt-dev <martin.abt.hst@gmail.com>
2026-05-09 14:00:45 +0200

    Delete pages/2_informations.py

--- pages/2_informations.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.write("Discount = Unit price - Unit sale price")
-st.write("Gross margin in % = (Unit sale price - Unit cost) / Unit sale price * 100")
-st.write("Profit = (Unit sale price - Unit cost) * Quantity")
-st.write("Revenue = Quantity * Unit sale price")
